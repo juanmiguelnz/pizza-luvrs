9a03cd90b048947c4d0f5d9189481df86acde594
Miguel <miguelkarlojuan@gmail.com>
2025-01-06 16:58:26 +1300

    fix typo

--- terraform/core-infra/network.tf
+++ terraform/core-infra/network.tf
@@ -76,7 +76,7 @@ resource "aws_security_group" "mgmtinstancesg" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allowssh" {
-  security_group_id = aws_security_group.example.id
+  security_group_id = aws_security_group.mgmtinstancesg.id
 
   cidr_ipv4   = "0.0.0.0/0"
   from_port   = 22
