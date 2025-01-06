2adb54b67f9455a3d175e882a0acd79b322389ff
Miguel <miguelkarlojuan@gmail.com>
2025-01-06 17:38:50 +1300

    access outputs

---
+++ terraform/pizza-infra/datasources.tf
@@ -0,0 +1,4 @@
+data "tfe_outputs" "core-infra" {
+  organization = "specialised-tfc"
+  workspace    = "core-infra-develop"
+}
--- terraform/pizza-infra/management.tf
+++ terraform/pizza-infra/management.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "mgmtvm" {
   ami                         = "ami-0ce4704e01dabf5a1"
   instance_type               = "t2.micro"
-  subnet_id                   = "subnet-00ce67d1525cc8ff4"
-  vpc_security_group_ids      = ["sg-04508fac444f9bac6"]
+  subnet_id                   = data.tfe_outputs.core-infra.nonsensitive_values.mgmtsubnet_id
+  vpc_security_group_ids      = data.tfe_outputs.core-infra.nonsensitive_values.mgmtinstancesg_id
   associate_public_ip_address = true
-  iam_instance_profile        = "EC2WithFullS3AccessRole"
+  iam_instance_profile        = "0PLoversInstanceProfile"
 
   tags = {
     Name = "mgmtvm"
