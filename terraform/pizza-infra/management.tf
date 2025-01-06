7a12414b3fbb37bea80827a1f9afcccd0a19dc46
Miguel <miguelkarlojuan@gmail.com>
2025-01-06 17:41:51 +1300

    set not string

--- terraform/pizza-infra/management.tf
+++ terraform/pizza-infra/management.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "mgmtvm" {
   ami                         = "ami-0ce4704e01dabf5a1"
   instance_type               = "t2.micro"
   subnet_id                   = data.tfe_outputs.core-infra.nonsensitive_values.mgmtsubnet_id
-  vpc_security_group_ids      = data.tfe_outputs.core-infra.nonsensitive_values.mgmtinstancesg_id
+  vpc_security_group_ids      = [data.tfe_outputs.core-infra.nonsensitive_values.mgmtinstancesg_id]
   associate_public_ip_address = true
   iam_instance_profile        = "0PLoversInstanceProfile"
 
