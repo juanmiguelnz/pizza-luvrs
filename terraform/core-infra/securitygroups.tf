112b4df08a68a36f2a98c62f0330a22b8c046b2c
Miguel <miguelkarlojuan@gmail.com>
2025-01-06 17:08:56 +1300

    sec groups in one file

---
+++ terraform/core-infra/securitygroups.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "mgmtinstancesg" {
+  name   = "mgmt-sg"
+  vpc_id = aws_vpc.core_vpc.id
+  tags = {
+    Name = "pizza"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allowssh" {
+  security_group_id = aws_security_group.mgmtinstancesg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+
+  tags = {
+    Name = "allow-ssh"
+  }
+}
