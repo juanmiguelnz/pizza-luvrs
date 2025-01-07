4f97b677f1776cc5afdef461b641d463ef3369bc
Miguel <miguelkarlojuan@gmail.com>
2025-01-07 20:45:30 +1300

    correct datasources path

--- terraform/core-infra/securitygroups.tf
+++ terraform/core-infra/securitygroups.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "mgmtinstancesg" {
 resource "aws_vpc_security_group_ingress_rule" "allowssh" {
   security_group_id = aws_security_group.mgmtinstancesg.id
 
-  cidr_ipv4   = "${(chomp(data.icanhazip.response_body))}/32"
+  cidr_ipv4   = "${(chomp(data.http.icanhazip.response_body))}/32"
   from_port   = 22
   ip_protocol = "tcp"
   to_port     = 22
--- terraform/pizza-infra/datasources.tf
+++ terraform/pizza-infra/datasources.tf
@@ -2,7 +2,3 @@ data "tfe_outputs" "core-infra" {
   organization = "specialised-tfc"
   workspace    = "core-infra-develop"
 }
-
-data "http" "icanhazip" {
-  url = "http://icanhazip.com"
-}
