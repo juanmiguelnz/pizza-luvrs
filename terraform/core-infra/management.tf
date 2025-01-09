fd9b07eea4412c824fb9ae8d258eeb66ff7d2e05
Miguel <miguelkarlojuan@gmail.com>
2025-01-09 15:52:17 +1300

    correct subnet and sg ids

--- terraform/core-infra/management.tf
+++ terraform/core-infra/management.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "mgmtvm" {
   ami                         = "ami-0ce4704e01dabf5a1"
   instance_type               = "t2.micro"
-  subnet_id                   = data.tfe_outputs.core-infra.nonsensitive_values.mgmtsubnet_id
-  vpc_security_group_ids      = [data.tfe_outputs.core-infra.nonsensitive_values.mgmtinstancesg_id]
+  subnet_id                   = aws_subnet.mgmtsubnet.id
+  vpc_security_group_ids      = [aws_security_group.mgmtinstancesg.id]
   associate_public_ip_address = true
   iam_instance_profile        = "0PLoversInstanceProfile"
 
   tags = {
     Name        = "mgmtvm"
     Environment = "develop"
-    Billing    = "123456789"
+    Billing     = "123456789"
   }
 }
