5b9c555933e52083611f3fd43cedad3ec5cc8b54
Miguel <miguelkarlojuan@gmail.com>
2025-01-10 14:28:34 +1300

    create lb

--- terraform/pizza-infra/main.tf
+++ terraform/pizza-infra/main.tf
@@ -3,17 +3,17 @@ resource "aws_lb" "pizza" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+  subnets            = [for subnet in data.tfe_outputs.core-infra.nonsensitive_values.public_subnets : subnet.id]
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.id
-    prefix  = "test-lb"
-    enabled = true
-  }
+  # access_logs {
+  #   bucket  = aws_s3_bucket.lb_logs.id
+  #   prefix  = "test-lb"
+  #   enabled = true
+  # }
 
   tags = {
-    Environment = "production"
+    Name = "${var.prefix}-lb"
   }
 }
