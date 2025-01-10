ccdc4067608b79ec7a32ab43a4f7b47d49007573
Miguel <miguelkarlojuan@gmail.com>
2025-01-10 13:12:43 +1300

    remove lb

--- terraform/pizza-infra/security.tf
+++ terraform/pizza-infra/security.tf
@@ -1,33 +1,33 @@
-# resource "aws_security_group" "lb_sg" {
-#   name   = "${var.prefix}-lb-sg"
-#   vpc_id = data.tfe_outputs.core-infra.nonsensitive_values.vpc_id
+# # resource "aws_security_group" "lb_sg" {
+# #   name   = "${var.prefix}-lb-sg"
+# #   vpc_id = data.tfe_outputs.core-infra.nonsensitive_values.vpc_id
+# #   tags = {
+# #     Name = "${var.prefix}-lb-sg"
+# #   }
+# # }
+
+# resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+#   security_group_id = aws_security_group.lb_sg.id
+
+#   cidr_ipv4   = "0.0.0.0/0"
+#   from_port   = 80
+#   ip_protocol = "tcp"
+#   to_port     = 80
+
 #   tags = {
-#     Name = "${var.prefix}-lb-sg"
+#     Name = "${var.prefix}-allow-http"
 #   }
 # }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_http" {
-  security_group_id = aws_security_group.lb_sg.id
+# resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+#   security_group_id = aws_security_group.lb_sg.id
 
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
+#   cidr_ipv4   = "0.0.0.0/0"
+#   from_port   = 443
+#   ip_protocol = "tcp"
+#   to_port     = 443
 
-  tags = {
-    Name = "${var.prefix}-allow-http"
-  }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_https" {
-  security_group_id = aws_security_group.lb_sg.id
-
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 443
-  ip_protocol = "tcp"
-  to_port     = 443
-
-  tags = {
-    Name = "${var.prefix}-allow-https"
-  }
-}
+#   tags = {
+#     Name = "${var.prefix}-allow-https"
+#   }
+# }
