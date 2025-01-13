bd4504d3084307bf06a4f8723333112fb19ed8f2
Miguel <miguelkarlojuan@gmail.com>
2025-01-13 17:20:53 +1300

    fix syntax

--- terraform/pizza-infra/datasources.tf
+++ terraform/pizza-infra/datasources.tf
@@ -7,11 +7,6 @@ data "http" "local_ip" {
   url = "https://api64.ipify.org?format=text" # Service that returns your public IP
 }
 
-output "current_ip" {
-  value = data.http.my_ip.response_body
-}
-
-
 data "aws_ssm_parameter" "pizza_db_name" {
   name = "pizza-db-name"
 }
