4211bb069564dff972e03cca43b5047dc439a8ee
Miguel <miguelkarlojuan@gmail.com>
2025-01-13 13:24:23 +1300

    fix syntax

--- terraform/pizza-infra/datasources.tf
+++ terraform/pizza-infra/datasources.tf
@@ -5,15 +5,12 @@ data "tfe_outputs" "core-infra" {
 
 data "aws_ssm_parameter" "pizza_db_name" {
   name = "pizza-db-name"
-  type = "SecureString"
 }
 
 data "aws_ssm_parameter" "pizza_db_user" {
   name = "pizza-db-user"
-  type = "SecureString"
 }
 
 data "aws_ssm_parameter" "pizza_db_pass" {
   name = "pizza-db-pass"
-  type = "SecureString"
 }
