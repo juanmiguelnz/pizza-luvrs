19506e9bcd4d68b2bc560fcca231e9b5718dfb52
Miguel <miguelkarlojuan@gmail.com>
2025-01-18 01:17:57 +1300

    not required

--- terraform/pizza-infra/security.tf
+++ terraform/pizza-infra/security.tf
@@ -71,9 +71,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_outgoing_traffic" {
   security_group_id = aws_security_group.web_servers_sg.id
 
   cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 0
   ip_protocol = -1
-  to_port     = 0
 
   tags = {
     Name = "${var.prefix}-allow-all-outgoing-traffic"
