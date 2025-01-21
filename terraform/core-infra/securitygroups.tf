b40ae34df00d15678a2a3c0e708366546a497338
Miguel <miguelkarlojuan@gmail.com>
2025-01-22 01:42:08 +1300

    allow 3000 from local

--- terraform/core-infra/securitygroups.tf
+++ terraform/core-infra/securitygroups.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "mgmtinstancesg" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allowssh" {
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.mgmtinstancesg.id
 
   cidr_ipv4   = "${(chomp(data.http.icanhazip.response_body))}/32"
@@ -18,6 +18,20 @@ resource "aws_vpc_security_group_ingress_rule" "allowssh" {
     Name = "${var.prefix}-allow-ssh"
   }
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_3000" {
+  security_group_id = aws_security_group.mgmtinstancesg.id
+
+  cidr_ipv4   = "${(chomp(data.http.icanhazip.response_body))}/32"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+
+  tags = {
+    Name = "${var.prefix}-allow-ssh"
+  }
+}
+
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_outgoing_traffic" {
   security_group_id = aws_security_group.mgmtinstancesg.id
