14e9fb61c842dbf55115b35dc492f3d7befeddd0
Miguel <miguelkarlojuan@gmail.com>
2025-01-22 01:50:00 +1300

    allow port 80

--- terraform/core-infra/securitygroups.tf
+++ terraform/core-infra/securitygroups.tf
@@ -32,6 +32,18 @@ resource "aws_vpc_security_group_ingress_rule" "allow_3000" {
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_80" {
+  security_group_id = aws_security_group.mgmtinstancesg.id
+
+  cidr_ipv4   = "${(chomp(data.http.icanhazip.response_body))}/32"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+
+  tags = {
+    Name = "${var.prefix}-allow-http"
+  }
+}
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_outgoing_traffic" {
   security_group_id = aws_security_group.mgmtinstancesg.id
