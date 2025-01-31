0812240c4300df2fc970e3ccc1068c6a2a6e8098
Miguel <miguelkarlojuan@gmail.com>
2025-01-31 19:20:01 +1300

    force destroy

--- terraform/pizza-infra/s3.tf
+++ terraform/pizza-infra/s3.tf
@@ -6,6 +6,7 @@ resource "random_string" "random" {
 
 resource "aws_s3_bucket" "pizza" {
   bucket = "p-love-nz"
+  force_destroy = true
 
   tags = {
     Name = "${var.prefix}-${random_string.random.result}"
