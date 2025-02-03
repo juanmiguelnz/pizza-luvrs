d198fef67502745fd48acb0bef73aa7471ff45ba
Miguel <miguelkarlojuan@gmail.com>
2025-02-04 00:36:05 +1300

    add ssh access to ec2

--- terraform/core-infra/instanceprofile.tf
+++ terraform/core-infra/instanceprofile.tf
@@ -29,6 +29,11 @@ resource "aws_iam_role_policy_attachment" "dynamodb_full_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
+resource "aws_iam_role_policy_attachment" "ssm_read_only_access" {
+  role       = aws_iam_role.pizza.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
+
 resource "aws_iam_instance_profile" "pizza" {
   name = "${var.prefix}-profile"
   role = aws_iam_role.pizza.name
