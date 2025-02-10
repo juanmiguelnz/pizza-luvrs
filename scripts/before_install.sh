4ee06e4dbfdf26dfacc18ecc0d95a6cc0279e855
Miguel <miguelkarlojuan@gmail.com>
2025-02-10 17:09:22 +1300

    add code deploy

---
+++ scripts/before_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e  # Exit if any command fails
+
+# Execute commands directly inside the script
+echo "Running BeforeInstall commands..."
+
+sudo yum update -y
+sudo yum install -y nodejs
+
+# Alternative inline execution using a one-liner
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.bashrc
+nvm install 18
+nvm use 18
+node -v
+npm -v
